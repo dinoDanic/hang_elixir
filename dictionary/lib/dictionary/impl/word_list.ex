ed9f87efef2185bc30ffaae39ffcad9e366ccc0e
Dino Danic <dino.danic@gmail.com>
2024-02-03 18:12:49 +0100

    im back..

--- dictionary/lib/dictionary/impl/word_list.ex
+++ dictionary/lib/dictionary/impl/word_list.ex
@@ -2,9 +2,9 @@ defmodule Dictionary.Impl.WordList do
   @type t :: list(String)
 
   @spec word_list() :: t
-
   def word_list do
-    "assets/words.txt"
+    "../../../assets/words.txt"
+    |> Path.expand(__DIR__)
     |> File.read!()
     |> String.split(~r/\n/, trim: true)
   end
